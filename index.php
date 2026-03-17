b5f26653938f17d3874066bc64341f238c16593c
MaciejS <56941288+MaciejSzelag@users.noreply.github.com>
2026-03-17 23:06:36 +0000

    Overview of Cryptographic Systems (400 words)

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>To do list</title>
+  <link rel="stylesheet" href="css/style.min.css">
 </head>
 <body>
     <h1>To do list</h1>
