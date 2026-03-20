a529e23b489323c9fd9f18a856f9be8f695094a9
MaciejS <56941288+MaciejSzelag@users.noreply.github.com>
2026-03-20 22:23:07 +0000

    index - todo list- initial structure - nav, form, list. CSS/SCSS - form and list style

--- index.php
+++ index.php
@@ -1,13 +1,73 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pl">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>To do list</title>
-  <link rel="stylesheet" href="css/style.min.css">
+    <title>My To Do List</title>
+    <link rel="stylesheet" href="css/style.min.css">
 </head>
 <body>
-    <h1>To do list</h1>
-    
+  <nav>
+       <div>
+    <p><?php echo date("D d F Y")?></p>
+   </div>
+   <div class="nav-list">
+     <ul>
+      <li><a href="">All tasks</a></li>
+      <li><a href="">Add</a></li>
+    </ul>
+   </div>
+
+  </nav>
+    <main>
+        <h1>To-Do List</h1>
+
+        <section class="section-form">
+            <form id="todo-form">
+                <label for="todo-input">Add new task</label>
+                <input type="text" id="todo-input" placeholder="Add a new task" required>
+                <button type="submit">Add</button>
+            </form>
+        </section>
+
+        <section class="section-todo-list">
+            <ul id="todo-list">
+              <li>
+                    <!-- <input type="checkbox" id="task-1"> -->
+                    <label for="task-1">Buy a milk</label>
+                    <button type="button">Delete</button>
+                </li><li>
+                    <!-- <input type="checkbox" id="task-1"> -->
+                    <label for="task-1">Buy a milk</label>
+                    <button type="button">Delete</button>
+                </li>
+                <li>
+                    <!-- <input type="checkbox" id="task-1"> -->
+                    <label for="task-1">Buy a milk</label>
+                    <button type="button">Delete</button>
+                </li>
+                <li>
+                    <!-- <input type="checkbox" id="task-1"> -->
+                    <label for="task-1">Buy a milk</label>
+                    <button type="button">Delete</button>
+                </li>
+                <li>
+                    <!-- <input type="checkbox" id="task-1"> -->
+                    <label for="task-1">Buy a milk</label>
+                    <button type="button">Delete</button>
+                </li>
+                <li>
+                    <!-- <input type="checkbox" id="task-1"> -->
+                    <label for="task-1">Buy a milk</label>
+                    <button type="button">Delete</button>
+                </li>
+                <li>
+                    <!-- <input type="checkbox" id="task-1"> -->
+                    <label for="task-1">Buy a milk</label>
+                    <button type="button">Delete</button>
+                </li>
+            </ul>
+        </section>
+    </main>
+
 </body>
 </html>
